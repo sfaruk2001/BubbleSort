7cda54cf81a3d49d5a9b3fa9734f4e8c6403809c
shihabfaruk <sfaruk3131p@gmail.com>
2022-08-28 15:43:00 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = {2, 8, 5, 3, 9, 4, 1};
+        int len = 7;
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr, int len) {
+
+        for (int i = 0; i < len -1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int swap = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = swap;
+                }
+            }
+        }
+    }
+
+
+    //this will check if any swaps occured during a pass. If none occurs the program can exit faster without doing extra loops.
+    public static void bubbleSort(int[] arr) {
+        int len = arr.length;
+        boolean swapped = false;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int swap = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = swap;
+                    swapped = true;
+                }
+            }
+            if (swapped = false) {
+                break;
+            }
+        }
+    }
+}
